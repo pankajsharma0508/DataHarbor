0a36abc1e04bf364c4dd1fa910976bf9c5da919d
Pankaj Sharma <pankajsharma0508@hotmail.com>
2024-12-24 23:34:05 +0100

    Added Repository.

--- DataHarbor.Extractors/Readers/CsvFileReader.cs
+++ DataHarbor.Extractors/Readers/CsvFileReader.cs
@@ -9,7 +9,24 @@ namespace DataHarbor.Extractors.Readers
 
         public List<ExpandoObject> ReadFile(string filePath)
         {
-            return [];
+            var result = new List<ExpandoObject>();
+            var lines = File.ReadAllLines(filePath);
+            if (lines?.Count() > 0)
+            {
+                foreach (var line in lines)
+                {
+                    var values = line.Split(',');
+                    var entry = new ExpandoObject();
+                    int columnIndex = 0;
+                    foreach (var value in values)
+                    {
+                        columnIndex++;
+                        (entry as IDictionary<string, object>)[$"column{columnIndex}"] = value;
+                    }
+                    result.Add(entry);
+                }
+            }
+            return result;
         }
     }
 }
