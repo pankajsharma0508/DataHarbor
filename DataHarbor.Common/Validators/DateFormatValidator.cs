dc94534055d091d595997f7b487d35cf03c544b3
Pankaj Sharma <pankajsharma0508@hotmail.com>
2025-02-10 22:14:40 +0100

    Added Validators.

---
+++ DataHarbor.Common/Validators/DateFormatValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace DataHarbor.Common.Validators
+{
+    public class DateFormatValidator : AbstractValidator<DateRequest>
+    {
+        private string DateFormat { get; set; }
+
+        public DateFormatValidator(string dateFormat, bool canBeEmpty = false)
+        {
+            DateFormat = dateFormat;
+            RuleFor(x => x.DateValue).NotEmpty()
+                .WithMessage("Date is Required.")
+                .Must(IsInValidFormat)
+                .WithMessage($"Invalid date format. Please {dateFormat}");
+        }
+
+        private bool IsInValidFormat(string date)
+        {
+            return DateTime.TryParseExact(date, DateFormat,
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out _);
+        }
+    }
+
+    public class DateRequest(string dateValue)
+    {
+        public string DateValue { get; set; } = dateValue;
+    }
+}
